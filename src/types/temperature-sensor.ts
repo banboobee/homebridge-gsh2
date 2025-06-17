80ead19802c18f1c0d624b87c3b63305eab3af28
banboobee <qzh00033@nifty.ne.jp>
2025-06-18 05:37:45 +0900

    Temperature sensor with HumiditySensor service.

--- src/types/temperature-sensor.ts
+++ src/types/temperature-sensor.ts
@@ -12,24 +12,39 @@ export class TemperatureSensor extends ghToHap implements ghToHap_t {
   }
 
   sync(service: ServiceType): SmartHomeV1SyncDevices {
+    const traits = [
+      'action.devices.traits.TemperatureControl',
+    ];
+    const attributes = {
+      queryOnlyTemperatureControl: true,
+      temperatureUnitForUX: this.hap.config.forceFahrenheit ? 'F' : 'C',
+    } as any;
+    if (service.serviceCharacteristics.find(x => x.uuid === Characteristic.CurrentRelativeHumidity)) {
+      traits.push('action.devices.traits.HumiditySetting');
+      attributes['queryOnlyHumiditySetting'] = true;
+    }
+    //console.log(JSON.stringify(traits));
+    //console.log(JSON.stringify(attributes, null, 2));
+
     return this.createSyncData(service, {
       type: 'action.devices.types.SENSOR',
-      traits: [
-        'action.devices.traits.TemperatureControl',
-      ],
-      attributes: {
-        queryOnlyTemperatureControl: true,
-        temperatureUnitForUX: this.hap.config.forceFahrenheit ? 'F' : 'C',
-      },
+      traits,
+      attributes,
     });
   }
 
   query(service: ServiceType) {
-    return {
+    const response = {
       online: true,
       temperatureSetpointCelsius: service.serviceCharacteristics.find(x => x.uuid === Characteristic.CurrentTemperature)?.value,
       temperatureAmbientCelsius: service.serviceCharacteristics.find(x => x.uuid === Characteristic.CurrentTemperature)?.value,
     } as any;
+    if (service.serviceCharacteristics.find(x => x.uuid === Characteristic.CurrentRelativeHumidity)) {
+      response['humidityAmbientPercent'] = service.serviceCharacteristics.find(x => x.uuid === Characteristic.CurrentRelativeHumidity)?.value;
+    }
+    //console.log(response);
+
+    return response;
   }
 
   async execute(service: ServiceType, command: SmartHomeV1ExecuteRequestCommands): Promise<SmartHomeV1ExecuteResponseCommands> {
