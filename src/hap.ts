4a286ae029653badf2d1043ee7c184ca8eb53a71
banboobee <qzh00033@nifty.ne.jp>
2025-06-30 05:53:06 +0900

    Reverted wrongly removed line.

--- src/hap.ts
+++ src/hap.ts
@@ -100,6 +100,7 @@ export class Hap {
     Characteristic.ActiveIdentifier,
     Characteristic.Mute,
     Characteristic.ContactSensorState,
+    Characteristic.OccupancyDetected,
     Characteristic.CurrentMediaState,
   ];
 
