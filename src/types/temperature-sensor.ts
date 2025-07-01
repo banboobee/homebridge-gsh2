4be8967e392ca3720e4f04660e05a0e596f83206
banboobee <qzh00033@nifty.ne.jp>
2025-07-01 19:03:59 +0900

    Added capacityRemaining query response.

--- src/types/temperature-sensor.ts
+++ src/types/temperature-sensor.ts
@@ -19,12 +19,18 @@ export class TemperatureSensor extends ghToHap implements ghToHap_t {
       queryOnlyTemperatureControl: true,
       temperatureUnitForUX: this.hap.config.forceFahrenheit ? 'F' : 'C',
     } as any;
+
+    // check if the sensor has the humidity characteristic
     if (service.serviceCharacteristics.find(x => x.uuid === Characteristic.CurrentRelativeHumidity)) {
       traits.push('action.devices.traits.HumiditySetting');
       attributes['queryOnlyHumiditySetting'] = true;
     }
-    //console.log(JSON.stringify(traits));
-    //console.log(JSON.stringify(attributes, null, 2));
+
+    // check if the sensor has the batteryLevel characteristic
+    if (service.serviceCharacteristics.find(x => x.uuid === Characteristic.BatteryLevel)) {
+      traits.push('action.devices.traits.EnergyStorage');
+      attributes['queryOnlyEnergyStorage'] = true;
+    }
 
     return this.createSyncData(service, {
       type: 'action.devices.types.SENSOR',
@@ -39,8 +45,28 @@ export class TemperatureSensor extends ghToHap implements ghToHap_t {
       temperatureSetpointCelsius: service.serviceCharacteristics.find(x => x.uuid === Characteristic.CurrentTemperature)?.value,
       temperatureAmbientCelsius: service.serviceCharacteristics.find(x => x.uuid === Characteristic.CurrentTemperature)?.value,
     } as any;
+
+    // check if the sensor has the humidity characteristic
     if (service.serviceCharacteristics.find(x => x.uuid === Characteristic.CurrentRelativeHumidity)) {
       response['humidityAmbientPercent'] = service.serviceCharacteristics.find(x => x.uuid === Characteristic.CurrentRelativeHumidity)?.value;
+    }
+    
+    // check if the sensor has the batteryLevel characteristic
+    if (service.serviceCharacteristics.find(x => x.uuid === Characteristic.BatteryLevel)) {
+      const descriptions = ['CRITICALLY_LOW', 'CRITICALLY_LOW', 'LOW', 'MEDIUM', 'HIGH', 'FULL', 'FULL'];
+      const thresholds = [0, 10, 20, 40, 80, 90, 100];
+      const lowBattery = !!service.serviceCharacteristics.find(x => x.uuid === Characteristic.StatusLowBattery)?.value;
+      const current = service.serviceCharacteristics.find(x => x.uuid === Characteristic.BatteryLevel)?.value as number;
+      const description = lowBattery ? descriptions[0] : descriptions[
+        thresholds.reduce((x, y, i) => {
+          return current >= y ? i : x;
+        }, 0)
+      ];
+      response['descriptiveCapacityRemaining'] = description,
+      response['capacityRemaining'] = [{
+        rawValue: current,
+        unit: 'PERCENTAGE',
+      }];
     }
     //console.log(response);
 
