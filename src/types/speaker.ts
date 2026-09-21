d991de4fdaab3c228e99d1d9f3e0b9ed1cf72b11
banboobee <qzh00033@nifty.ne.jp>
2026-09-21 16:56:33 +0900

    unified hierarchy managements.

--- src/types/speaker.ts
+++ src/types/speaker.ts
@@ -68,14 +68,6 @@ export class Speaker extends ghToHap implements ghToHap_t {
   query(service: ServiceType, primaryResponse?: Record<string, any>) {
     let response = {} as any;
 
-    // const primary = Speaker.primaryService[service.uniqueId];
-    // if (primary && !primaryResponse) {
-    //   // upward traversal to find a root node
-    //   response = this.hap.types[primary.type].query(primary);
-    //   response['id'] = primary.uniqueId; // responds as root node.
-    //   return response;
-    // }
-
     if (primaryResponse?.online === undefined) {
       response.online = true;
     }
